4ab67e1ea7ae48f949e90f179ee87c59a1a6d4ad
Asha jyothi <Aashajyothi-UI>
2022-04-26 11:27:35 +0530

    modifed files

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(express.static(__dirname + '/dist/app2'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/app2/index.html'));
+res.sendFile(path.join(__dirname+'/dist/app2'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8000);
